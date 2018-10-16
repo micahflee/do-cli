fc38c0323b2f44a6fb08da460d492ed5f581250e
Micah Lee <micah@micahflee.com>
2018-10-16 16:47:52 -0700

    Add support for listing digitalocean droplets

--- dodeploy.py
+++ dodeploy.py
@@ -2,24 +2,29 @@
 import os
 import sys
 import argparse
-import dopy
+from dopy.manager import DoManager
 
 def main():
     # Make sure we have a DigitalOcean API key
     if 'DO_API_KEY' not in os.environ:
         print("You must have a DigitalOcean API key stored in the DO_API_KEY environment variable")
         sys.exit()
+    do = DoManager(None, os.environ['DO_API_KEY'], api_version=2)
 
     # Parse arguments
     parser = argparse.ArgumentParser()
     group = parser.add_mutually_exclusive_group()
-    group.add_argument('--list', action="store_true", help="List DigitalOcean droplets in the 'ctf' project")
+    group.add_argument('--list', action="store_true", help="List droplets")
     group.add_argument('--create', action="store_true", help="Create a new droplet")
     group.add_argument('--delete', action="store_true", help="Delete a droplet")
     args = parser.parse_args()
 
     if args.list:
-        pass
+        droplets = do.all_active_droplets()
+        for droplet in droplets:
+             print("{} {}".format(droplet['name'], droplet['ip_address']))
+             print("id={} | {}MB memory, {}GB disk, {}".format(droplet['id'], droplet['memory'], droplet['disk'], droplet['region']['slug']))
+             print("")
 
     elif args.create:
         pass
