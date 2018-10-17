f2e87fc6ba79c3aa04088959da135a1236544e95
Micah Lee <micah@micahflee.com>
2018-10-17 12:00:36 -0700

    Make it so the arg parser uses sub-commands, with each command being able to have its own args

--- dodeploy.py
+++ dodeploy.py
@@ -7,46 +7,52 @@ from colored import fg, bg, attr
 from tabulate import tabulate
 
 
-def main():
-    # Make sure we have a DigitalOcean API key
-    if 'DO_API_KEY' not in os.environ:
-        print("You must have a DigitalOcean API key stored in the DO_API_KEY environment variable")
-        sys.exit()
-    do = DoManager(None, os.environ['DO_API_KEY'], api_version=2)
+# Make sure we have a DigitalOcean API key
+if 'DO_API_KEY' not in os.environ:
+    print("You must have a DigitalOcean API key stored in the DO_API_KEY environment variable")
+    sys.exit()
+do = DoManager(None, os.environ['DO_API_KEY'], api_version=2)
 
+
+def list(args):
+    table = []
+    headers = ['Droplet', 'IP address', 'id', 'Memory', 'Disk', 'Region']
+
+    droplets = do.all_active_droplets()
+    for droplet in droplets:
+        table.append([
+            "{}{}{}".format(attr('bold'), droplet['name'], attr('reset')),
+            droplet['ip_address'],
+            droplet['id'],
+            droplet['memory'],
+            droplet['disk'],
+            droplet['region']['slug']
+        ])
+    print(tabulate(table, headers=headers))
+    print("")
+
+
+def create(args):
+    print("create not implemented")
+
+
+def delete(args):
+    print("delete not implemented")
+
+
+def main():
     # Parse arguments
     parser = argparse.ArgumentParser()
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument('--list', action="store_true", help="List droplets")
-    group.add_argument('--create', action="store_true", help="Create a new droplet")
-    group.add_argument('--delete', action="store_true", help="Delete a droplet")
+    subparsers = parser.add_subparsers(dest='command')
+    subparsers.required = True
+    list_parser = subparsers.add_parser('list', help='List droplets')
+    list_parser.set_defaults(func=list)
+    create_parser = subparsers.add_parser('create', help='Create a new droplet')
+    create_parser.set_defaults(func=create)
+    delete_parser = subparsers.add_parser('delete', help='Delete a droplet')
+    delete_parser.set_defaults(func=delete)
     args = parser.parse_args()
-
-    if args.list:
-        table = []
-        headers = ['Droplet', 'IP address', 'id', 'Memory', 'Disk', 'Region']
-
-        droplets = do.all_active_droplets()
-        for droplet in droplets:
-            table.append([
-                "{}{}{}".format(attr('bold'), droplet['name'], attr('reset')),
-                droplet['ip_address'],
-                droplet['id'],
-                droplet['memory'],
-                droplet['disk'],
-                droplet['region']['slug']
-            ])
-        print(tabulate(table, headers=headers))
-        print("")
-
-    elif args.create:
-        pass
-
-    elif args.delete:
-        pass
-
-    else:
-        parser.print_help()
+    args.func(args)
 
 
 if __name__ == '__main__':
